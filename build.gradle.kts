37e939d4f1f99b1c607f9993c05dabe72f4c0dad
Ibrahim Tasyurt <ibrahim.tasyurt@udemy.com>
2020-06-28 14:50:03 +0300

    Implement custom event for cloud bus 1. Adds rabbit to docker config 2. Adds related dependencies 3. Custom event related logic

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,8 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.cloud:spring-cloud-starter-bus-amqp:2.2.2.RELEASE")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
--- src/main/kotlin/org/itasyurt/lbsandbox/LbsandboxApplication.kt
+++ src/main/kotlin/org/itasyurt/lbsandbox/LbsandboxApplication.kt
@@ -1,12 +1,17 @@
 package org.itasyurt.lbsandbox
 
+
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Primary
+import org.springframework.cloud.bus.BusProperties
+import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Component
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class LbsandboxApplication
@@ -19,17 +24,34 @@ fun main(args: Array<String>) {
 @RequestMapping("/api")
 class DummyController {
 
-    var data = 10
+    @Autowired
+    lateinit var applicationContext: ApplicationContext
+
+    @Autowired
+    lateinit var busProperties: BusProperties
+
+    @Autowired
+    lateinit var state:State
 
     @RequestMapping("", method = [RequestMethod.GET])
     fun getResult(): Int {
-        return data
+        return state.data
     }
 
     @RequestMapping("", method = [RequestMethod.PUT])
     fun updateData(@RequestParam newData: Int) {
-        this.data = newData
+        val id = busProperties.id
+        val event = UpdateDataEvent(this, id, newData)
+        applicationContext.publishEvent(event)
+
     }
+
+}
+
+@Component
+class State {
+
+    var data = 10
 
 }
 
